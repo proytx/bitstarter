1a5bf3da8e4a9a957058f0c80496b25145b617fa
proytx <partha.roy.tx@att.net>
2013-07-11 17:21:22 +0000

    i Please enter the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+// need to add Node filesystem support
+var objFs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+app.get('/', function(request, response) {A
+  // Open the file buffer (can handle more than just ascii)
+  var objBufFile = objFs.readFileSync('index.html');
+  // Commented out for HW 3 index.html input
+  //response.send('Hello World2!');
+  // Added this line instead to send response back from index.html
+  // Docs say default encoding is UTF-8
+  response.send(objBufFile.toString());
 });
 
 var port = process.env.PORT || 5000;
